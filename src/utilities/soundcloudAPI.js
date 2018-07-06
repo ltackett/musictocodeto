85bc49e1ff5d64df4612550cab0d78c08409648a
ltackett <lorin.tackett@gmail.com>
2018-07-06 12:31:19 -0700

    Adds SoundCloud API endpoint for /resolve

--- src/utilities/soundcloudAPI.js
+++ src/utilities/soundcloudAPI.js
@@ -1,4 +1,24 @@
+import axios from 'axios'
+
+const apiRoot = '//api.soundcloud.com'
+const apiKey = process.env.REACT_APP_SOUNDCLOUD_API_KEY || ''
+
+const api = axios.create({
+  baseURL: apiRoot,
+  params: {
+    client_id: apiKey
+  }
+})
+
 export default {
-  root: '//api.soundcloud.com',
-  key: process.env.REACT_APP_SOUNDCLOUD_API_KEY || '',
+  root: apiRoot,
+  key: apiKey,
+
+  paths: {
+    resolve: (url) => api.get(`${apiRoot}/resolve`, {
+      params: {
+        url: `http://soundcloud.com/${url}`
+      }
+    }),
+  }
 }
