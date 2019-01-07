fe3ca2981c72013cbdf5274f6cc957d12a57a659
ltackett <lorin.tackett@gmail.com>
2019-01-07 12:52:12 -0800

    Adds about cmd

---
+++ src/programs/about.js
@@ -0,0 +1,27 @@
+import React, { Fragment as R} from 'react';
+import { store } from 'store'
+import { stdout as output } from 'modules/stdout/actions'
+
+import avatar from 'textblocks/avatar'
+import theme from 'utilities/theme'
+
+import H from 'Components/Text_Highlight'
+import MR from 'Components/Text_MagicalRainbow'
+const K = ({ children }) => <H color={theme.orange}>{children}</H>
+
+const { dispatch } = store
+const stdout = (o) => dispatch(output(o))
+
+const about = (cmdObject) => new Promise((resolve, reject) => {
+  if (cmdObject.params.filter(p => p === '-v' || p === '--verbose').length > 0) {
+    stdout(avatar)
+  }
+
+  stdout([<R><H>MTCT</H> is a labor of love built by <MR>Lorin Tackett</MR>.</R>])
+
+  resolve()
+});
+
+
+
+export default about;
--- src/programs/index.js
+++ src/programs/index.js
@@ -1,9 +1,11 @@
+import about from 'programs/about';
 import fork from 'programs/fork';
 import help from 'programs/help';
 import userinfo from 'programs/userinfo';
 import player from 'programs/player'
 
 const programs = {
+  about,
   fork,
   help,
   userinfo,
