42f38af3ed8df93089b81114f7b28a5644e01398
ltackett <lorin.tackett@gmail.com>
2019-01-23 09:23:11 -0800

    Adds spread/rest to get children and passed-in props to CTX

--- src/Containers/GlobalContext.js
+++ src/Containers/GlobalContext.js
@@ -3,13 +3,14 @@ const GlobalContext = React.createContext('GlobalContext')
 
 export default GlobalContext
 
-export const CTX = ({ children, component }) => {
-  const Component = component
+export const CTX = (props) => {
+  const Component = props.component
+  const { children, ...passedProps } = props
 
   return (
     <GlobalContext.Consumer>
       {(context) => (
-        <Component {...context}>
+        <Component {...context} {...passedProps}>
           {children}
         </Component>
       )}
