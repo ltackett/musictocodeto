d33e0f48225b2e589b44bb7cc056d48f664041b2
ltackett <lorin.tackett@gmail.com>
2019-02-16 20:27:09 -0800

    Removes console.log accidentally left in

--- src/programs/fs/ls.js
+++ src/programs/fs/ls.js
@@ -2,9 +2,6 @@ import getDirectoryFromPath from 'utilities/getDirectoryFromPath'
 import getPlaylistsFromGenre from 'utilities/getPlaylistsFromGenre'
 
 const ls = (cmdObject, { fs, path, stdout }) => new Promise((resolve, reject) => {
-  console.log({ type: getDirectoryFromPath(fs, path).type });
-
-
   if (getDirectoryFromPath(fs, path).type === 'genre') {
     stdout(getPlaylistsFromGenre(fs, path))
   } else {
