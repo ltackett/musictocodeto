c8a7efd54555232009f7bd9ed05fdee3b3da1186
ltackett <lorin.tackett@gmail.com>
2019-02-26 00:47:33 -0800

    Removes janky attempt at circumventing  autoplay user ineraction rules

--- src/index.js
+++ src/index.js
@@ -6,17 +6,5 @@ import 'index.css'
 
 import registerServiceWorker from 'registerServiceWorker';
 
-const eventFire = (el, etype) => {
-  if (el.fireEvent) {
-    el.fireEvent('on' + etype);
-  } else {
-    var evObj = document.createEvent('Events');
-    evObj.initEvent(etype, true, false);
-    el.dispatchEvent(evObj);
-  }
-}
-
-eventFire(document, 'click');
-
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
