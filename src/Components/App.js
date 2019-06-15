de8a5e5f8a20e392cd0a3c9ab6f67d2cc89c902d
ltackett <lorin.tackett@gmail.com>
2019-06-15 12:33:15 -0700

    Replaces BrowserRouter with HashRouter

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,5 +1,5 @@
 import React  from 'react'
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
+import { HashRouter as Router, Route, Switch, Link } from 'react-router-dom'
 
 import GlobalContext from 'Contexts/Global'
 import { RootStyles, Center } from 'Components/Styles'
